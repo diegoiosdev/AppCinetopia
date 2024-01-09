646b8fc82b6548fd59600dc66563e02a017c518a
Diego Fernando Siqueira Silva <diegofernando.ssiqueira@gmail.com>
2024-01-09 17:37:00 -0300

    refactor

--- CineTopia/cells/FavoriteMoviesCollectionViewCell.swift
+++ CineTopia/cells/FavoriteMoviesCollectionViewCell.swift
@@ -1,17 +1,7 @@
 import UIKit
 import Kingfisher
 
-internal class FavoriteMoviesCollectionViewCell: UIViewController {
-    
-    let movie = Movie(id: 1, title: "50 Tons de cinza", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQngF0a4kjzl55XwagnDqJDSJ_un1LxUs6-ekUUVDQ19OiOcG0oLPxcnGegCidjExU-KyU&usqp=CAU", synopsis: "A estudante de literatura Anastasia Steele, de 21 anos, entrevista o jovem bilionário Christian Grey, como um favor a sua colega de quarto Kate Kavanagh. Ela vê nele um homem atraente e brilhante, e ele fica igualmente fascinado por ela. Embora seja sexualmente inexperiente, Anastasia mergulha de cabeça nessa relação e descobre os prazeres do sadomasoquismo, tornando-se o objeto de submissão do enigmático Grey.", rate: 9.8, releaseDate: "12/02/2015")
-    
-    
-    private lazy var mainView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .white
-        return view
-    }()
+internal class FavoriteMoviesCollectionViewCell: UICollectionViewCell {
     
     private lazy var imageViewCollection: UIImageView = {
         let imageView = UIImageView()
@@ -45,45 +35,39 @@ internal class FavoriteMoviesCollectionViewCell: UIViewController {
 //     navigationController?.pushViewController(MoviesViewController(), animated: false)
      }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor(named: "BackgroundColor")
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor(named: "BackgroundColor")
         addSubviews()
         setupConstrains()
-        setupView(movie)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func addSubviews() {
-        view.addSubview(mainView)
-        mainView.addSubview(imageViewCollection)
-        mainView.addSubview(labelMovie)
-        mainView.addSubview(favoriteButton)
+        addSubview(imageViewCollection)
+        addSubview(labelMovie)
+        addSubview(favoriteButton)
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
-    }
-    
+
     private func setupConstrains() {
         NSLayoutConstraint.activate([
-            mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            mainView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            mainView.heightAnchor.constraint(equalToConstant: 200),
-            mainView.widthAnchor.constraint(equalToConstant: 115),
             
-            imageViewCollection.topAnchor.constraint(equalTo: mainView.topAnchor),
-            imageViewCollection.leadingAnchor.constraint(equalTo: mainView.leadingAnchor),
-            imageViewCollection.trailingAnchor.constraint(equalTo: mainView.trailingAnchor),
+            imageViewCollection.topAnchor.constraint(equalTo: topAnchor),
+            imageViewCollection.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageViewCollection.trailingAnchor.constraint(equalTo: trailingAnchor),
             imageViewCollection.heightAnchor.constraint(equalToConstant: 140),
             
             labelMovie.topAnchor.constraint(equalTo: imageViewCollection.bottomAnchor, constant: 10),
-            labelMovie.leadingAnchor.constraint(equalTo: mainView.leadingAnchor),
-            labelMovie.trailingAnchor.constraint(equalTo: mainView.trailingAnchor),
+            labelMovie.leadingAnchor.constraint(equalTo: leadingAnchor),
+            labelMovie.trailingAnchor.constraint(equalTo: trailingAnchor),
             
             favoriteButton.topAnchor.constraint(equalTo: labelMovie.bottomAnchor, constant: 8),
             favoriteButton.heightAnchor.constraint(equalToConstant: 25),
             favoriteButton.widthAnchor.constraint(equalToConstant: 25),
-            favoriteButton.centerXAnchor.constraint(equalTo: mainView.centerXAnchor),
+            favoriteButton.centerXAnchor.constraint(equalTo: centerXAnchor),
         ])
     }
     
