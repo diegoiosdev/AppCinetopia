bafd628187632158acd18d3e0d491d5b3a2f02e7
Diego Fernando Siqueira Silva <diegofernando.ssiqueira@gmail.com>
2024-01-09 18:30:54 -0300

    rename paste

---
+++ CineTopia/Controllers/FavoriteMoviesViewController.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+internal class FavoriteMoviesViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 14, left: 20, bottom: 14, right: 20)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(FavoriteMoviesCollectionViewCell.self, forCellWithReuseIdentifier: "FavoriteMoviesCollectionViewCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        addSubviews()
+        setupConstrains()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    private func addSubviews() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setupConstrains() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
+
+extension FavoriteMoviesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteMoviesCollectionViewCell", for: indexPath) as? FavoriteMoviesCollectionViewCell else {
+            fatalError("Erro ao renderizar")
+        }
+        
+        let currentMovie = movies[indexPath.item]
+        cell.setupView(currentMovie)
+        return cell
+        
+    }
+}
+
+extension  FavoriteMoviesViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/3, height: 200)
+    }
+}
